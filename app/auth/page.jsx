bc628276de9618e5ed7ecb6298b49ca274c849d1
aryan-srivastava1625 <ap9140@srmist.edu.in>
2025-04-20 16:34:40 +0530

    made the tables in supabase

---
+++ app/auth/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Page() {
+  return (
+    <div>
+      <div>
+        <img src="/logo.png" alt="logo" width={400} height={100}
+        className='w-[180px]'
+        
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Page;
